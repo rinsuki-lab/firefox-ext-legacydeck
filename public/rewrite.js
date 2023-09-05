49281ee96cd7386b6fca19ff4242b81351c359fa
rinsuki <428rinsuki+git@gmail.com>
2023-09-06 08:26:11 +0900

    use const headers

--- public/rewrite.js
+++ public/rewrite.js
@@ -2,16 +2,12 @@
 /// <reference types="web-ext-types" />
 console.log("LegacyDeck Boot", new Date())
 
-/** @type {Map<string, Headers>} */
-const map = new Map()
-
 const EXPECTED_SHA512_OF_HTML = "a9f733d3a17586262534de28faf4201bc9674e759730088bd58daaaa667c7855cd9748a6cb5510e30f7338ad2a95b0b9919c6093c8fe84c0579959031d078fc3"
 
 // Replace body with old one
 browser.webRequest.onBeforeRequest.addListener(async details => {
     console.log("got", details.url)
     const res = await fetch("https://web.archive.org/web/20221231161219id_/https://tweetdeck.twitter.com/", { credentials: "omit" })
-    map.set(details.requestId, res.headers)
     const resArrayBuffer = await res.arrayBuffer()
     const resArrayBufferHash = await crypto.subtle.digest("SHA-512", resArrayBuffer)
     const resArrayBufferHashString = Array.from(new Uint8Array(resArrayBufferHash)).map(x => x.toString(16).padStart(2, "0")).join("")
@@ -35,12 +31,17 @@ browser.webRequest.onHeadersReceived.addListener(details => {
     const ret = {
         responseHeaders: [
             { name: "content-type", value: "text/html; charset=UTF-8" },
-            ...Array.from(map.get(details.requestId)).map(([name_, value]) => {
-                const name = name_.toLowerCase()
-                if (!name.startsWith("x-archive-orig-")) return null
-                if (name === "x-archive-orig-content-length") return null
-                return { name: name.slice("x-archive-orig-".length), value }
-            }).filter(x => x != null),
+            { name: "x-frame-options", value: "SAMEORIGIN" },
+            { name: "content-security-policy", value: [
+                "default-src 'self'",
+                "connect-src 'self' blob: https://api.twitter.com https://upload.twitter.com https://ton.twimg.com https://api-ssl.bitly.com",
+                "font-src 'self' https://ton.twimg.com data:",
+                "frame-src https:",
+                "img-src https: data:",
+                "media-src blob: https://video.twimg.com https://ton.twimg.com",
+                "script-src 'self' 'unsafe-eval' https://*.twitter.com https://*.twimg.com https://api-ssl.bitly.com",
+                "style-src 'self' 'unsafe-inline' https://platform.twitter.com https://ton.twimg.com"
+            ].join("; ")},
         ],
         // responseHeaders: [{name: "location", value: "https://tweetdeck.twitter.com/#"}]
     }
